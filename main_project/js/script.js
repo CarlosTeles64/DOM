d0ae26d64fec0e768257e9b49702d215482bcaa0
Carlos Teles <carlosGteles64@gmail.com>
2020-07-31 10:15:08 -0300

    Module Separation

---
+++ main_project/js/script.js
@@ -0,0 +1,9 @@
+import initTabNav from './module/animations/initTabNav.js'
+import initScrollSuave from './module/animations/initScrollSuave.js'
+import initAnimacaoScroll from './module/animations/initAnimacaoScroll.js'
+import initAccordion from './module/animations/initAccordion.js'
+
+initTabNav()
+initScrollSuave()
+initAnimacaoScroll()
+initAccordion()
